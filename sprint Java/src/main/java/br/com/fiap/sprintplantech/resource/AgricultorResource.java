3e8cefe5f564069bf8202dfdc8346646991f8fbe
gabriel-bacelar-valentim <gabrielvalentimxd@gmail.com>
2024-05-08 11:25:43 -0300

    consegui fazer o resource

--- sprint Java/src/main/java/br/com/fiap/sprintplantech/resource/AgricultorResource.java
+++ sprint Java/src/main/java/br/com/fiap/sprintplantech/resource/AgricultorResource.java
@@ -3,34 +3,85 @@ package br.com.fiap.sprintplantech.resource;
 import br.com.fiap.sprintplantech.dto.request.AgricultorRequest;
 import br.com.fiap.sprintplantech.dto.response.AgricultorResponse;
 import br.com.fiap.sprintplantech.entity.Agricultor;
-import br.com.fiap.sprintplantech.repository.AgricultorRepository;
+import br.com.fiap.sprintplantech.service.AgricultorService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/agricultores")
 public class AgricultorResource implements ResourceDTO<AgricultorRequest, AgricultorResponse>{
 
     @Autowired
-    private AgricultorRepository repo;
+    private AgricultorService service;
 
-    @GetMapping
-    public List<Agricultor> findAll(){
-        return repo.findAll();
-    }
 
+    @Override
     @GetMapping(value = "/{id}")
-    public Agricultor findById(@PathVariable Long id){
-        return repo.findById(id).orElseThrow();
+    public ResponseEntity<AgricultorResponse> findById(@PathVariable Long id) {
+        var entity  = service.findById( id );
+        if (Objects.isNull( id )) return ResponseEntity.notFound().build();
+        var response = service.toResponse(entity);
+        return ResponseEntity.ok(response);
     }
 
     @Transactional
     @PostMapping
-    public Agricultor save(@RequestBody Agricultor agricultor) {
-        agricultor.setId(null);
-        return repo.save(agricultor);
+    @Override
+    public ResponseEntity<AgricultorResponse> save(@RequestBody @Valid AgricultorRequest r) {
+        var entity = service.toEntity( r );
+        var saved = service.save( entity );
+
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(entity.getId())
+                .toUri();
+
+                var response = service.toResponse(saved);
+        return ResponseEntity.created(uri).body(response);
     }
+
+    @GetMapping
+    public ResponseEntity<Collection<AgricultorResponse>> findAll(
+
+            @RequestParam(name = "agricultor.nome", required = false) String nome,
+            @RequestParam(name = "agricultor.cidade", required = false) String cidade,
+            @RequestParam(name = "agricultor.estado", required = false) String estado,
+            @RequestParam(name = "agricultor.telefone", required = false) String telefone,
+            @RequestParam(name = "agricultor.email", required = false) String email
+
+    ) {
+
+        var agricultor = Agricultor.builder()
+                .nome(nome)
+                .cidade(cidade)
+                .estado(estado)
+                .telefone(telefone)
+                .email(email)
+                .build();
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matchingAll()
+                .withIgnoreNullValues()
+                .withIgnoreCase();
+
+        Example<Agricultor> example = Example.of(agricultor, matcher);
+        Collection<Agricultor> agricultores = service.findAll(example);
+
+        var response = agricultores.stream().map(service::toResponse).toList();
+        return ResponseEntity.ok(response);
+
+    }
+
+
 }
--- sprint Java/src/main/java/br/com/fiap/sprintplantech/resource/ResourceDTO.java
+++ sprint Java/src/main/java/br/com/fiap/sprintplantech/resource/ResourceDTO.java
@@ -2,10 +2,12 @@ package br.com.fiap.sprintplantech.resource;
 
 import org.springframework.http.ResponseEntity;
 
-public interface ResourceDTO<Entity, Request, Response> {
+public interface ResourceDTO<Request, Response> {
 
     ResponseEntity<Response> findById(Long id);
 
     ResponseEntity<Response> save(Request r);
+
+
 
 }
